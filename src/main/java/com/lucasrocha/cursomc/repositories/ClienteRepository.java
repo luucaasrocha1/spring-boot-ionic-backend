f3b6a8d5062433635fc4b1199b7dc73328b4400f
Lucas Rocha <luucaasrocha@gmail.com>
2020-02-29 12:49:36 -0300

    Cliente, TipoCliente, Telefones e Enderecos

---
+++ src/main/java/com/lucasrocha/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.lucasrocha.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucasrocha.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
