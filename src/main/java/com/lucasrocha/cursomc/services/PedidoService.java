e319d2d193c43fb8baad4a72db8211f6ed1223e7
Lucas Rocha <luucaasrocha@gmail.com>
2020-04-07 20:58:26 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/lucasrocha/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.lucasrocha.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasrocha.cursomc.domain.Pedido;
+import com.lucasrocha.cursomc.repositories.PedidoRepository;
+import com.lucasrocha.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		}
+}
