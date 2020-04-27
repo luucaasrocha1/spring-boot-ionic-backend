b28b3d2258cb827ea0bd1a3b558ef63bde896798
Lucas Rocha <luucaasrocha@gmail.com>
2020-04-27 19:48:02 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/lucasrocha/cursomc/services/validation/ClienteInsert.java
+++ src/main/java/com/lucasrocha/cursomc/services/validation/ClienteInsert.java
@@ -14,8 +14,8 @@ import javax.validation.Payload;
 
 public @interface ClienteInsert {
 	String message() default "Erro de validação";
-	
+
 	Class<?>[] groups() default {};
-	
+
 	Class<? extends Payload>[] payload() default {};
 }
--- src/main/java/com/lucasrocha/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/lucasrocha/cursomc/services/validation/ClienteInsertValidator.java
@@ -12,25 +12,24 @@ import com.lucasrocha.cursomc.resources.exception.FieldMessage;
 import com.lucasrocha.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
 	@Override
-	
 	public void initialize(ClienteInsert ann) {
 	}
 
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
-		
+
 		List<FieldMessage> list = new ArrayList<>();
 
-		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCpf(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfouCnpj", "CPF inválido"));
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
-		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCnpj(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfouCnpj", "CNPJ inválido"));
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
-		
-		
+
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
